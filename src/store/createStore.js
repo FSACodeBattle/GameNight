885f5f983919138d84e7483851e6735ce247a410
John Yom <johnyom87@gmail.com>
2017-01-26 18:24:55 -0500

    working on createLogger

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -1,8 +1,8 @@
-import { applyMiddleware, compose, createStore } from 'redux'
-import thunk from 'redux-thunk'
-import { browserHistory } from 'react-router'
-import makeRootReducer from './reducers'
-import { updateLocation } from './location'
+import { applyMiddleware, compose, createStore } from 'redux';
+import thunk from 'redux-thunk';
+import { browserHistory } from 'react-router';
+import makeRootReducer from './reducers';
+import { updateLocation } from './location';
 
 export default (initialState = {}) => {
   // ======================================================
