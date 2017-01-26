2236bb670081ad494bbb15f6a953d7f1ddd3c19c
John Yom <johnyom87@gmail.com>
2017-01-26 18:29:41 -0500

    created createLogger for state

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -3,12 +3,13 @@ import thunk from 'redux-thunk';
 import { browserHistory } from 'react-router';
 import makeRootReducer from './reducers';
 import { updateLocation } from './location';
+import createLogger from 'redux-logger';
 
 export default (initialState = {}) => {
   // ======================================================
   // Middleware Configuration
   // ======================================================
-  const middleware = [thunk]
+  const middleware = [thunk, createLogger()]
 
   // ======================================================
   // Store Enhancers
