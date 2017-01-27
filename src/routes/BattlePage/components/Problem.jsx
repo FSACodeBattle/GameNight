d838862001d85bcaaf90657e08573f93c4db70a8
pchmura <pchmura8@gmail.com>
2017-01-27 16:35:42 -0500

    adding battle page

---
+++ src/routes/BattlePage/components/Problem.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+export const Problem = () => (
+  <div>
+  	<h2> Problem </h2>
+  	<p>"Write a program that prints the numbers from 1 to 100. But for multiples of three print “Fizz” instead of the number and for the multiples of five print “Buzz”. For numbers which are multiples of both three and five print “FizzBuzz”."</p>
+
+  </div>
+)
+
+export default Problem;
---
+++ src/routes/BattlePage/containers/BattlePageContainer.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import Problem from '../components/Problem'
+
+
+export const BattlePage = () => (
+  <div>
+  	<Problem />
+
+  </div>
+)
+
+export default BattlePage;
