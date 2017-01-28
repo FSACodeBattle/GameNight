c43d1db5b434836eceb62529f8e9e0856f18d305
John Yom <johnyom87@gmail.com>
2017-01-27 21:48:56 -0500

    mixing with master

--- src/containers/AppContainer.js
+++ src/containers/AppContainer.js
@@ -1,33 +1,61 @@
-import React, { Component } from 'react'
-import { browserHistory, Router, Route, IndexRoute } from 'react-router'
-import { Provider, connect } from 'react-redux';
-import CoreLayout from '../layouts/CoreLayout';
-import Home from '../routes/Home';
-import CodeEditor from '../routes/CodeEditor';
+// import React, { Component } from 'react'
+// import { browserHistory, Router, Route, IndexRoute } from 'react-router'
+// import { Provider, connect } from 'react-redux';
+// import CoreLayout from '../layouts/CoreLayout';
+// import Home from '../routes/Home';
+// import CodeEditor from '../routes/CodeEditor';
+
+// class AppContainer extends Component {
+//   shouldComponentUpdate () {
+//     return false
+//   }
+
+//   render () {
+//     const { store, routes } = this.props;
+//     console.log('~~~~~~~~~~~~~~~~STATE', this.state)
+//     console.log('~~~~~~~~~~~~~~~~props', this.props)
+
+//     return (
+//       <Provider store={store} >
+//         <Router history={browserHistory} routes={routes} >
+//           <Route path="/" component={CoreLayout} />
+//           <Route path="code_editor" component={CodeEditor} />
+//         </Router>
+//       </Provider>
+//     );
+//   }
+// }
+
+// // function mapStateToProps(state){
+// //   return state
+// // }
+
+// export default AppContainer;
+import React, { Component, PropTypes } from 'react'
+import { browserHistory, Router } from 'react-router'
+import { Provider } from 'react-redux'
 
 class AppContainer extends Component {
+  // static propTypes = {
+  //   routes : PropTypes.array.isRequired,
+  //   store  : PropTypes.object.isRequired
+  // }
+
   shouldComponentUpdate () {
     return false
   }
 
   render () {
-    const { store, routes } = this.props;
-    console.log('~~~~~~~~~~~~~~~~STATE', this.state)
-    console.log('~~~~~~~~~~~~~~~~props', this.props)
+    const { routes, store } = this.props
 
     return (
-      <Provider store={store} >
-        <Router history={browserHistory} routes={routes} >
-          <Route path="/" component={CoreLayout} />
-          <Route path="code_editor" component={CodeEditor} />
-        </Router>
+      <Provider store={store}>
+        <div style={{ height: '100%' }}>
+          <Router history={browserHistory} children={routes} />
+        </div>
       </Provider>
-    );
+    )
   }
 }
 
-// function mapStateToProps(state){
-//   return state
-// }
-
-export default AppContainer;
+export default AppContainer
