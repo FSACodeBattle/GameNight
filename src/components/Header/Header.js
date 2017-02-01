c7c58c0cbd369e52e57a9a182993084be760327c
John Yom <johnyom87@gmail.com>
2017-02-01 15:26:19 -0500

    small changes

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -13,18 +13,6 @@ export const Header = () => (
         <ul className="nav navbar-nav">
           <li><Link to="battlePage"><a href="#" />Battle Page</Link></li>
           <li><Link to="code_editor"><a href="#" />Code Editor</Link></li>
-          <li className="dropdown">
-            <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
-            <ul className="dropdown-menu">
-              <li><a href="#">Action</a></li>
-              <li><a href="#">Another action</a></li>
-              <li><a href="#">Something else here</a></li>
-              <li role="separator" className="divider"></li>
-              <li><a href="#">Separated link</a></li>
-              <li role="separator" className="divider"></li>
-              <li><a href="#">One more separated link</a></li>
-            </ul>
-          </li>
         </ul>
         <form className="navbar-form navbar-left search" id="login">
           <div className="form-group">
