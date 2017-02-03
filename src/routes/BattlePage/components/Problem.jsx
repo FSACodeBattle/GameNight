80e5c705d02559c70904112af20ce24cb4c80e8a
pchmura <pchmura8@gmail.com>
2017-02-03 14:30:04 -0500

    adding comments

--- src/routes/BattlePage/components/Problem.jsx
+++ src/routes/BattlePage/components/Problem.jsx
@@ -1,32 +1,22 @@
 import React from 'react';
 import './Problem.scss';
 
-// let question = 	`Write a program that prints the numbers from 1 to 100.\n
-// But for multiples of three print "Fizz" instead of the number and for the multiples of five print "Buzz".\n
-// For numbers which are multiples of both three and five print “FizzBuzz”.`
 
-// const questionArray = question.split("\n");
 
 export const Problem = (props) => {
 
+	// get the current question that was passed in through props
 	const currentQuestion = props.CurrentQuestion; 
-	//const questionArray = currentQuestion.split("\n");
-			//<h2> Problem </h2>
-			//{
-			 	//questionArray && questionArray.map((questionLine, i) => (
-			 		//<p key={i} style={{textAlign: 'left'}}>{questionLine}</p>
-			 		//)
-			 	//)
-			//}
-	
-			if (currentQuestion){
-				var splitQuestion = currentQuestion.questionText.split("\n")
-				 var pTags = splitQuestion.map((questionLine, i) => (
-			 		<p key={i} style={{textAlign: 'left'}}>{questionLine}</p>
-			 		))
-			}
-			currentQuestion && currentQuestion.questionText 
-
+			
+		//if the current question is defined 
+		if (currentQuestion){
+			//split the qurstion text by the next line tags
+			var splitQuestion = currentQuestion.questionText.split("\n")
+			//map over each "line" of text and wrap them in <p> tags
+			var pTags = splitQuestion.map((questionLine, i) => (
+		 		<p key={i} style={{textAlign: 'left'}}>{questionLine}</p>
+		 	))
+		}
 
 	return(
 
@@ -38,8 +28,5 @@ export const Problem = (props) => {
 		)
 
 }
-
-  
-
 
 export default Problem;
