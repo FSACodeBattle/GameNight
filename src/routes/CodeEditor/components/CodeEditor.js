4a6cc308bf45697046e06b8343dc58cbeef3db32
Mitch Kosowski <mitchkosowski@gmail.com>
2017-02-09 16:15:14 -0500

    removed some unnecessary variables

--- src/routes/CodeEditor/components/CodeEditor.js
+++ src/routes/CodeEditor/components/CodeEditor.js
@@ -25,18 +25,12 @@ class CodeEditor extends Component {
   }
 
   componentDidMount(){
-
     // update playerNumber here using sockets
     // this is dummy code for now
     const startingTime = Date.now();
     this.setState({
-      playerNumber: 1,
       startingTime: startingTime,
-
     })
-
-
-
   }
 
   updateCode(newCode) {
@@ -47,17 +41,11 @@ class CodeEditor extends Component {
 
   handleSubmit() {
     const startingTime = this.state.startingTime;
-    const playerNumber = this.state.playerNumber;
-    const playerProgress = this.state.playerProgress;
     console.log("inside handleSubmit");
     console.log(socket);
     axios.post('/api/code', {
       code: this.state.code,
       timeElapsed: (Date.now() - startingTime)/1000,
-      // use playerNumber in playerProgress array
-      // to figure out where you are in tests
-      playerNumber: this.state.playerNumber,
-      playerProgress: this.state.playerProgress,
       currentQuestionID: this.props.currentQuestionID.questionID,
       room: this.props.roomID,
       socketID: socket.id
@@ -93,12 +81,6 @@ class CodeEditor extends Component {
       <div id="code-editor-text" className="container">
         <div className="row">
           <div className="col-xs-12">
-        {/*removed the 0-0 player progress above the code editor*/}
-            {/*<div className="playerProgress" style={{color: "#777"}}>
-              <div>
-                {this.state.playerProgress.join('-')}
-              </div>
-            </div>*/}
             <div id="enterCodeText"style={{color:"#777"}}>Enter Code
             </div>
             <CodeMirror
@@ -126,37 +108,4 @@ class CodeEditor extends Component {
   }
 }
 
-
-
-
 export default CodeEditor;
-
-
-//       <div id="code-editor-text">
-//         <div className="container">
-//           <div className="row">
-//             <div className="col-xs-12 col-md-8 col-md-offset-2" id="code-offset">
-//               <div className="playerProgress" style={{color: "#777"}}>
-//                   <div>
-//                     {this.state.playerProgress.join('-')}
-//                   </div>
-//               </div>
-//               <div id="enterCodeText"style={{color:"#777"}}>Enter Code
-//               </div>
-//               <CodeMirror
-//                 value={this.state.code}
-//                 onChange={this.updateCode}
-//                 options={options}
-//               />
-//               <button id="submit-btn" className="btn btn-primary btn-small" onClick={this.handleSubmit}>SUBMIT</button>
-//               <div id="enterCodeText"style={{color:"#777"}}>
-//                 Answer
-//               </div>
-//               <CodeMirror
-//                 value={this.state.results}
-//                 options={optionsForAnswers}
-//               />
-//             </div>
-//           </div>
-//         </div>
-//       </div>
