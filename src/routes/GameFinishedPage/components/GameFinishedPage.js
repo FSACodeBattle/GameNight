a8c1aec2076dd8871c2d756640e9a59410c2afeb
John Yom <johnyom87@gmail.com>
2017-02-12 04:53:17 -0500

    gamewon and gameFinished, still ugly

--- src/routes/GameFinishedPage/components/GameFinishedPage.js
+++ src/routes/GameFinishedPage/components/GameFinishedPage.js
@@ -1,14 +1,47 @@
-import React from 'react'
-// import FireworksImage from '../assets/fireworks.gif'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import './GameFinishedPage.scss';
 
-export const GameFinishedPage = () => (
-  <div>
-    <h1>You lost the game, but at least you finished all the questions :D</h1>
-    <img
-      alt='At least you tried'
-      className='atLeastYouTried'
-      src='http://ci.memecdn.com/2000578.gif' />
-  </div>
-)
+class GameFinishedPage extends Component {
+  constructor(){
+    super()
+  }
+  render(){
+    const { ownAnswers, opponentAnswers } = this.props
+    return (
+      <div className="container" id="gameWonPage">
+        <div className="row">
+          <h1>You lost the game, but at least you finished all the questions :D</h1>
+         <img
+             alt='At least you tried'
+             className='atLeastYouTried'
+             src='http://ci.memecdn.com/2000578.gif' />
 
-export default GameFinishedPage
+        </div>
+        <div className="row" id="answersRow">
+          <div id="userAnswers">
+            My Answers:
+             {
+               ownAnswers && ownAnswers.map(answer => <div key={answer.id}>{answer}</div>)
+             }
+          </div>
+          <div id="userAnswers">
+            Opponents Answers:
+             {
+               opponentAnswers && opponentAnswers.map(answer => <div key={answer.id}>{answer}</div>)
+             }
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps(state){
+  return {
+    ownAnswers: state.matchResults.ownAnswers,
+    opponentAnswers: state.matchResults.opponentAnswers
+  }
+}
+
+export default connect(mapStateToProps)(GameFinishedPage);
--- src/routes/GameWonPage/components/GameWonPage.js
+++ src/routes/GameWonPage/components/GameWonPage.js
@@ -1,19 +1,47 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import './GameWonPage.scss';
 
-export const GameWonPage = () => {
-  return (
-    <div className="container" id="gameWonPage">
-      <div className="row">
-        <h1>Congratulations You Won!!</h1>
-        <img
-          alt='Fireworks!'
-          className='fireworks'
-          src='http://bestanimations.com/Holidays/Fireworks/fireworks/ba-large-white-firework-gif-pic.gif' />
+class GameWonPage extends Component {
+  constructor(){
+    super()
+  }
+  render(){
+    const { ownAnswers, opponentAnswers } = this.props
+    return (
+      <div className="container" id="gameWonPage">
+        <div className="row">
+          <h1>Congratulations You Won!!</h1>
+          <img
+            alt='Fireworks!'
+            className='fireworks'
+            src='http://bestanimations.com/Holidays/Fireworks/fireworks/ba-large-white-firework-gif-pic.gif' />
+
+        </div>
+        <div className="row" id="answersRow">
+          <div id="userAnswers">
+            My Answers:
+             {
+               ownAnswers && ownAnswers.map(answer => <div key={answer.id}>{answer}</div>)
+             }
+          </div>
+          <div id="userAnswers">
+            Opponents Answers:
+             {
+               opponentAnswers && opponentAnswers.map(answer => <div key={answer.id}>{answer}</div>)
+             }
+          </div>
+        </div>
       </div>
-    </div>
-  )
+    )
+  }
 }
 
-export default GameWonPage;
+function mapStateToProps(state){
+  return {
+    ownAnswers: state.matchResults.ownAnswers,
+    opponentAnswers: state.matchResults.opponentAnswers
+  }
+}
 
+export default connect(mapStateToProps)(GameWonPage);
