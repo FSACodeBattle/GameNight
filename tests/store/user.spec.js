0f57f3006ebe342cb6ffe416bd6ada13a7c4c1df
Ming Tung Wong <mingtungwong@outlook.com>
2017-02-13 10:42:01 -0500

    Fix failing tests

--- tests/store/user.spec.js
+++ tests/store/user.spec.js
@@ -17,23 +17,5 @@ describe('(Internal Module) User', () => {
     it('Should initialize users as an empty object', () => {
       expect(usersReducer(undefined, {})).to.deep.equal({user: {}});
     })
-
-
-    // it('Should return the previous state if an action was not matched.', () => {
-    //   var socket = {}
-    //   socket.emit = function() {};
-    //   let state = usersReducer(undefined, {});
-    //   expect(state).to.deep.equal({user: {}});
-    //   state = usersReducer(state, {type: 'aaa'});
-    //   expect(state).to.deep.equal({user: {}});
-    //
-    //   const user = {username: 'test'};
-    //   state = usersReducer(state, setUser(user));
-    //   expect(state).to.deep.equal(user);
-    //   state = usersReducer(state, {type: 'bbb'});
-    //   expect(state).to.deep.equal({user});
-    // })
-    //Cannot get any tests to work that requires setUser since it does a
-    //socket.emit
   })
 })
