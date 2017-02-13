b4c4d740110a82b91c6431803f9b3f5ac8ca1739
John Yom <johnyom87@gmail.com>
2017-02-13 03:40:31 -0500

    changed layout of gamewon and gamelost

--- src/routes/GameWonPage/components/GameWonPage.js
+++ src/routes/GameWonPage/components/GameWonPage.js
@@ -9,32 +9,31 @@ class GameWonPage extends Component {
   render(){
     const { ownAnswers, opponentAnswers } = this.props
     return (
-      <div className="container" id="gameFinished">
-        <h1>Congratulations You Won!!</h1>
-        <div className="row" id="answersRow">
-          <div className="col-md-6 col-md-offset-3" id="fireworks">
-            <img
-              alt="Fireworks!"
-              className="img-responsive"
-              src='http://bestanimations.com/Holidays/Fireworks/fireworks/ba-large-white-firework-gif-pic.gif'
-            />
+      <div className="container">
+        <div className="row" id="headRow">
+          <div className="col col-xs-12 col-md-6 col-lg-12">
+            <div className="heady">
+              <img src="http://bestanimations.com/Holidays/Fireworks/fireworks/ba-large-white-firework-gif-pic.gif" className="fireworks" />
+              <span className="slogan">Congrautions You Won</span>
+            </div>
           </div>
         </div>
         <div className="row">
-          <div className="col-md-5" id="myanswer">
-            <div id="answersheader">
-              My Answers:
-            </div>
-             {
-                ownAnswers && ownAnswers.map((answer, i = 1) =>
-                  <div>
-                    <div key={answer.id + i} style={{fontSize: '20px', color: '#777'}}>
-                      Question {++i}:
+          <div className="boxy">
+            <div className="col-md-5" id="myanswer">
+              <div id="answersheader">
+                My Answers:
+              </div>
+               {
+                  ownAnswers && ownAnswers.map((answer, i = 1) =>
+                    <div>
+                      <div key={answer.id + i} style={{fontSize: '20px', color: 'black'}}>
+                        Question {++i}:
+                      </div>
+                      <div key={answer.id}>{answer}</div>
                     </div>
-                    <div key={answer.id}>{answer}</div>
-                  </div>
-                )
-             }
+                  )
+               }
           </div>
           <div className="col-md-5 col-md-offset-2"  id="userAnswers">
               <div id="answersheader">
@@ -43,7 +42,7 @@ class GameWonPage extends Component {
                 {
                  opponentAnswers && opponentAnswers.map((answer, i = 1) =>
                     <div>
-                      <div key={answer.id + i} style={{fontSize: '20px', color: '#777'}}>
+                      <div key={answer.id + i} style={{fontSize: '20px', color: 'black'}}>
                         Question {++i}:
                       </div>
                       <div key={answer.id}>{answer}</div>
@@ -53,8 +52,9 @@ class GameWonPage extends Component {
           </div>
         </div>
       </div>
-    )
-  }
+    </div>
+  )
+}
 }
 
 function mapStateToProps(state) {
@@ -65,3 +65,52 @@ function mapStateToProps(state) {
 }
 
 export default connect(mapStateToProps)(GameWonPage);
+
+// dfasd
+// {/*
+//       {/*<div className="container" id="gameFinished">
+//         <h1>Congratulations You Won!!</h1>
+//         <div className="row" id="answersRow">
+//           <div className="col-md-6 col-md-offset-3" id="fireworks">
+//             <img
+//               alt="Fireworks!"
+//               className="img-responsive"
+//               src='http://bestanimations.com/Holidays/Fireworks/fireworks/ba-large-white-firework-gif-pic.gif'
+//             />
+//           </div>
+//         </div>*/}
+//       //   <div className="row">
+//       //     <div className="col-md-5" id="myanswer">
+//       //       <div id="answersheader">
+//       //         My Answers:
+//       //       </div>
+//       //        {
+//       //           ownAnswers && ownAnswers.map((answer, i = 1) =>
+//       //             <div>
+//       //               <div key={answer.id + i} style={{fontSize: '20px', color: '#777'}}>
+//       //                 Question {++i}:
+//       //               </div>
+//       //               <div key={answer.id}>{answer}</div>
+//       //             </div>
+//       //           )
+//       //        }
+//       //     </div>
+//       //     <div className="col-md-5 col-md-offset-2"  id="userAnswers">
+//       //         <div id="answersheader">
+//       //           Opponent's Answers:
+//       //         </div>
+//       //           {
+//       //            opponentAnswers && opponentAnswers.map((answer, i = 1) =>
+//       //               <div>
+//       //                 <div key={answer.id + i} style={{fontSize: '20px', color: '#777'}}>
+//       //                   Question {++i}:
+//       //                 </div>
+//       //                 <div key={answer.id}>{answer}</div>
+//       //               </div>
+//       //             )
+//       //           }
+//       //     </div>
+//       //   </div>
+//       // </div>
+//       // d
+// // s*/}
